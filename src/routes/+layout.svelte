bc6faa601a996bf027281a9907bc71365962b536
Gabriele Corti <borntofrappe@gmail.com>
2024-09-12 21:23:52 +0200

    update root layout
    for the landing page don't display the navigation back to the root route, but a bold header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
-  import '../app.css'
+  import { page } from "$app/stores";
+  import "../app.css";
   import svg from "$lib/svg";
   let { children } = $props();
 </script>
 
 <div class="layout">
-  <nav>
-    <a href="/">
-      <span class="visually-hidden">P-Ball</span>
+  {#if $page.url.pathname === "/"}
+    <header>
+      <p class="visually-hidden">P-ball</p>
       {@html svg["banner"]}
-    </a>
-  </nav>
+      <p>Pushing you to play <b>Pokémon Pinball</b> and collect them all</p>
+    </header>
+  {:else}
+    <nav>
+      <a href="/">
+        <span class="visually-hidden">P-Ball</span>
+        {@html svg["banner"]}
+      </a>
+    </nav>
+  {/if}
 
   <main>
     {@render children()}
