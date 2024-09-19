9987a4f04a8e59f0713cdcf1c781e4b3799ae7e3
Gabriele Corti <borntofrappe@gmail.com>
2024-09-19 18:20:43 +0200

    build test

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,5 +1,6 @@
+import { dev } from "$app/environment";
 import Database from "better-sqlite3";
-import { DB_PATH } from "$env/static/private";
+import { DB_NAME } from "$env/static/private";
 import type {
   SearchParam,
   Version,
@@ -10,7 +11,7 @@ import type {
   Location,
 } from "./types";
 
-const db = new Database(DB_PATH);
+const db = dev ? new Database(`static/${DB_NAME}`) : new Database(`/${DB_NAME}`);
 
 const getImgSrc = (arrayBuffer: ArrayBuffer): string => {
   return `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
