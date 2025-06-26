937f6111bac966a3e7f67c7c522aa0580dab282d
Gabriele Corti <borntofrappe@gmail.com>
2025-06-26 14:58:59 +0200

    select entry by name

--- app/entry/[name].tsx
+++ app/entry/[name].tsx
@@ -2,13 +2,23 @@ import Entry from "@/components/Entry";
 import Panel from "@/components/Panel";
 import Ribbon from "@/components/Ribbon";
 import { Colors } from "@/constants/Colors";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { useSQLiteContext } from "expo-sqlite";
-import React, { useEffect, useState } from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const EntryByName = () => {
   const db = useSQLiteContext();
+  const router = useRouter();
+  const page = useRef<ScrollView>(null);
+
   const { name } = useLocalSearchParams<{ name: string }>();
   const [entry, setEntry] = useState<Entry>();
   const [locations, setLocations] = useState<{ Red: Area[]; Blue: Area[] }>({
@@ -18,6 +28,22 @@ const EntryByName = () => {
   const [connections, setConnections] = useState<
     { name: string; uri: string }[]
   >([]);
+
+  const selectEntryByName = async (name: string) => {
+    if (entry && entry.name === name) {
+      return page.current?.scrollTo({
+        y: 0,
+        animated: true,
+      });
+    }
+
+    router.push({
+      pathname: "/entry/[name]",
+      params: {
+        name,
+      },
+    });
+  };
 
   useEffect(() => {
     db.getFirstAsync<EntryDB>(
@@ -140,6 +166,7 @@ const EntryByName = () => {
     <>
       <Ribbon />
       <ScrollView
+        ref={page}
         contentContainerStyle={{
           flex: 1,
           maxWidth: 500,
@@ -202,28 +229,32 @@ const EntryByName = () => {
           {connections.length > 1 && (
             <Panel label="EVO" theme="Yellow">
               {connections.map((connection) => (
-                <View
-                  style={[styles.itemsContainer]}
+                <Pressable
+                  onPress={() => {
+                    selectEntryByName(connection.name);
+                  }}
                   key={`${connection.name}`}
                 >
-                  <Image
-                    style={{
-                      width: 46,
-                      height: 30,
-                    }}
-                    source={{ uri: connection.uri }}
-                  />
-                  <Text
-                    style={[
-                      styles.itemsText,
-                      {
-                        color: Colors.panel.Yellow.color,
-                      },
-                    ]}
-                  >
-                    {connection.name}
-                  </Text>
-                </View>
+                  <View style={[styles.itemsContainer]}>
+                    <Image
+                      style={{
+                        width: 46,
+                        height: 30,
+                      }}
+                      source={{ uri: connection.uri }}
+                    />
+                    <Text
+                      style={[
+                        styles.itemsText,
+                        {
+                          color: Colors.panel.Yellow.color,
+                        },
+                      ]}
+                    >
+                      {connection.name}
+                    </Text>
+                  </View>
+                </Pressable>
               ))}
             </Panel>
           )}
