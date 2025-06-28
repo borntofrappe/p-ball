099ac8d47adea64a92e31efeb72cb5942ed3e586
Gabriele Corti <borntofrappe@gmail.com>
2025-06-28 10:03:50 +0200

    export animations

--- constants/Animations.ts
+++ constants/Animations.ts
@@ -24,3 +24,90 @@ export const animationError = {
   duration: 500,
   delay: 1000,
 };
+
+export const animationNotFound = {
+  image: require("@/assets/images/entry-not-found.png"),
+  size: 150,
+  steps: 3,
+  keyframe: new Keyframe({
+    0: {
+      transform: [
+        {
+          translateX: 0,
+        },
+      ],
+    },
+    19.99: {
+      transform: [
+        {
+          translateX: 0,
+        },
+      ],
+    },
+    20: {
+      transform: [
+        {
+          translateX: -150,
+        },
+      ],
+    },
+    39.99: {
+      transform: [
+        {
+          translateX: -150,
+        },
+      ],
+    },
+    40: {
+      transform: [
+        {
+          translateX: 0,
+        },
+      ],
+    },
+    59.99: {
+      transform: [
+        {
+          translateX: 0,
+        },
+      ],
+    },
+    60: {
+      transform: [
+        {
+          translateX: -150,
+        },
+      ],
+    },
+    79.99: {
+      transform: [
+        {
+          translateX: -150,
+        },
+      ],
+    },
+    80: {
+      transform: [
+        {
+          translateX: 0,
+        },
+      ],
+    },
+    99.99: {
+      transform: [
+        {
+          translateX: 0,
+        },
+      ],
+    },
+    100: {
+      transform: [
+        {
+          translateX: -300,
+        },
+      ],
+    },
+  }),
+  duration: 4000,
+  delay: 100,
+};
