6d8708da1493dc72821a89ab9da0850acd7a3f3b
Gabriele Corti <borntofrappe@gmail.com>
2025-06-29 12:08:04 +0200

    remove extra type

--- app/catch.tsx
+++ app/catch.tsx
@@ -16,12 +16,6 @@ import {
 } from "react-native";
 const imagePaddle = require("@/assets/images/catch-paddle.png");
 
-type Guess = {
-  no: string;
-  name: string;
-  category: string;
-  uri: string;
-};
 const Catch = () => {
   const db = useSQLiteContext();
 
@@ -40,10 +34,13 @@ const Catch = () => {
 
   const textInput = useRef<TextInput>(null);
   const [reveal, setReveal] = useState<boolean>(false);
-  const [guess, setGuess] = useState<Guess>({
+  const [guess, setGuess] = useState<Entry>({
     no: " ",
     name: " ",
     category: " ",
+    height: -1,
+    weight: -1,
+    description: " ",
     uri: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAeCAMAAABkHdyoAAAASFBMVEWoqKj4+vj6+Pj4+fj4+Pv5+Pj4+Pr4+Pn4+Piqqqqoq6ipqqmoqqqoqqirqKipqaiqqKqoqaqqqKioqaipqKmpqKioqKqoqKkc5p6VAAABI0lEQVR42qWUjVbDIAyFr8VtqYo6YuD931Sh/KSpdMfjd3bWQG+TNCEFGHs8DkT8Ce1E8JBwFptBBS3m/BvSVIN4D1BHBWfssolf2LgSuWs26OYcPcqwObV2YsTh/a1sFQU629Lwruyn/W0pet9qz+pOWSn1Z/MvOHL/LbbULdt46amqnoy9CUQL2NRkyz7AJxg4S+orrCuRcf/Ra1r/qLP+kK+OMpMuUWFRcjLykZH0sJNkYA6PJyXRZgkdBGIiEMUxPrIdXJt65FSfiNRrkw6nJuHIhdrIhBw6ZPkWqO7btj7b5y+Qs7FbRk8SHNHZWEe24+ExgYeGhiHZR0td7Ll8abqliG3KehkZob+hoxs0upYcMCNEf9QH4FV9DLlYd/ybb9YCDv4mNKpOAAAAAElFTkSuQmCC",
   });
 
